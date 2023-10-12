1dac09316d877335ab34578fc65465ea88bb6a4e
unknown <nami0879@naver.com>
2023-10-12 19:58:52 +0900

    cors

--- src/main/react-library/src/App.js
+++ src/main/react-library/src/App.js
@@ -8,18 +8,18 @@ function App() {
   // const setData = useState(null)[1];
   const [data, setData] = useState(null);
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  // 첫 화면 렌더링과 의존성 배열이 변경될 때마다 실행
+  // useEffect(() => {
 
-  const fetchData = async () => {
-    try {
-      const response = await fetch('/hello');
-      const result = await response.text();
-      setData(result);  // setData를 써서 업데이트 하는 이유는 옵저버 패턴으로 바뀐 값만 렌더링하여 성능을 최적화하기 위함.
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
+  // }, [data]);
+
+  const hello = async () => {
+    const response = await fetch('http://localhost:8080/hello', {
+        method: 'GET',
+        mode: 'cors'
+    });
+    const result = await response.text();
+    setData(result);
   };
 
   return (
@@ -30,6 +30,7 @@ function App() {
             {data}
         </DefaultLayout>
       </pre>
+      <button onClick={hello}>hello</button>
     </div>
   );
 }
