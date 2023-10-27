a8e3f41aa4b945d29b66e7706c4d0466b086d8f1
unknown <nami0879@naver.com>
2023-10-27 17:32:37 +0900

    home toggle dropdown with react way

--- front-react-app/src/templates/fragments/Header.tsx
+++ front-react-app/src/templates/fragments/Header.tsx
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import home from '../../assets/imgs/home.png'
 
 import '../../stylesheets/common/common.css';
-import '../../stylesheets/layouts/header.css';
+import '../../stylesheets/fragments/header.css';
 
 const Header: React.FC = () => {
+  // React Hook인 useState를 사용하여 새로운 상태 변수를 생성
+  // 변수명 isDropdownVisible, 초깃값 false.
+  // 이 변수를 변경하기 위한 함수 setDropdownVisible.
+  const [isDropdownVisible, setDropdownVisible] = useState(false);
+
+  const toggleDropdown = () => {
+    // setDropdownVisible(true)를 호출하면 isDropdownVisible 값이 true
+    // setDropdownVisible(false)를 호출하면 isDropdownVisible 값이 false    
+    setDropdownVisible(!isDropdownVisible);
+  };
+
   return (
     <header>
-      <div className='dropdown-container'>
+      <div id='header-container' onClick={toggleDropdown}>
         <img src={home} alt='home' />
-        <ul className='dropdown-content'>
-          {/* React-Router의 Link component를 통해 URL이 변경 */}
-          <li><Link to="/SignIn">sign in</Link></li>
-          <li><Link to="/SignUp">sign up</Link></li>
-        </ul>
+        <div id='header-contents-box' style={{ display: isDropdownVisible ? 'flex' : 'none' }}>
+          <ul id='header-contents'>
+            {/* React-Router의 Link component를 통해 URL이 변경 */}
+            <li><Link to="/SignIn">Sign In</Link></li>
+            <li><Link to="/SignUp">Sign Up</Link></li>
+          </ul>
+        </div>
       </div>
     </header>
   );
--- front-react-app/src/templates/fragments/Nav.tsx
+++ front-react-app/src/templates/fragments/Nav.tsx
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom';
 import menu from '../../assets/imgs/menu.png'
 
 import '../../stylesheets/common/common.css';
-import '../../stylesheets/layouts/nav.css';
+import '../../stylesheets/fragments/nav.css';
 
 const Nav: React.FC = () => {
   return (
     <nav>
-      <div className='dropdown-container'>
+      <div id='nav-container'>
         <img src={menu} alt='menu' />
-        <ul className='dropdown-content'>
+        <ul id='nav-contents'>
           <li><Link to="/Menu1">Menu1</Link></li>
           <li><Link to="/Menu2">Menu2</Link></li>
           <li><Link to="/Menu3">Menu3</Link></li>
