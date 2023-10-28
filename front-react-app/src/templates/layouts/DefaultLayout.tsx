8447bf023caf42ff08751c81a54b77211b0bd6ba
unknown <nami0879@naver.com>
2023-10-28 15:01:27 +0900

    nav-contents css

--- front-react-app/src/templates/layouts/DefaultLayout.tsx
+++ front-react-app/src/templates/layouts/DefaultLayout.tsx
@@ -28,12 +28,15 @@ const Layout: React.FC<LayoutProps> = (props) => {
 
       {/* nav-contents html 요소를 Nav.tsx에서 제어하기에는 코드가 복잡해짐
       DefaultLayout에 html 요소를 넣고 display 요소를 제어하는 것이 간편 */}
-      <ul id='nav-contents' style={{ left: isNavOpen ? '0' : navWidthMinus }}>
-        <li><Link to="/Menu1">Menu1</Link></li>
-        <li><Link to="/Menu2">Menu2</Link></li>
-        <li><Link to="/Menu3">Menu3</Link></li>
-        <li><Link to="/Menu4">Menu4</Link></li>
-      </ul>
+      <div id='nav-contents-box' style={{ left: isNavOpen ? '0' : navWidthMinus }}>
+        <h4 id='nav-greetings'>Welcome</h4>
+        <ul id='nav-contents'>
+          <li><Link to="/Menu1">Menu1</Link></li>
+          <li><Link to="/Menu2">Menu2</Link></li>
+          <li><Link to="/Menu3">Menu3</Link></li>
+          <li><Link to="/Menu4">Menu4</Link></li>
+        </ul>
+      </div>
 
       {/* <main style={{ marginLeft: isNavOpen ? navWidth : '0' }}> */}
       <main style={{ left: isNavOpen ? navWidth : '0' }}>
