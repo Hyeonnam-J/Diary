61099c59e443ed34c89d3008157164602fbb783a
unknown <nami0879@naver.com>
2023-10-29 20:37:49 +0900

    test code test

--- src/main/java/com/hn/api/diary/controller/SignController.java
+++ src/main/java/com/hn/api/diary/controller/SignController.java
@@ -2,6 +2,8 @@ package com.hn.api.diary.controller;
 
 import com.hn.api.diary.service.SignService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +16,14 @@ public class SignController {
 
     private final SignService signService;
 
+    @GetMapping(value = "/test")
+    public String test(String str){
+        return str;
+    }
+
     @PostMapping(value = "/signUp")
     public void signUp(@RequestBody SignUpDTO signUpDTO){
         signService.signUp(signUpDTO);
     }
+
 }
--- src/main/java/com/hn/api/diary/dto/SignUpDTO.java
+++ src/main/java/com/hn/api/diary/dto/SignUpDTO.java
@@ -1,5 +1,6 @@
 package com.hn.api.diary.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
@@ -8,4 +9,9 @@ public class SignUpDTO {
   private String userId;
   private String password;
 
+  @Builder
+  public SignUpDTO(String userId, String password) {
+    this.userId = userId;
+    this.password = password;
+  }
 }
