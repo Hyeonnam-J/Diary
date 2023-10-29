569bc248bade1cdf36be67311c25d8f0cc0482bf
unknown <nami0879@naver.com>
2023-10-29 23:08:06 +0900

    put signUp data at hibernate with test code

--- src/main/java/com/hn/api/diary/dto/SignUpDTO.java
+++ src/main/java/com/hn/api/diary/dto/SignUpDTO.java
@@ -9,6 +9,10 @@ public class SignUpDTO {
   private String userId;
   private String password;
 
+  /*
+  테스트 코드에서 데이터를 주기 위한 용도.
+  클래스에 빌더 어노테이션을 달면 다른 어노테이션들과 충돌이 있는 경우가 있다고 함.
+   */
   @Builder
   public SignUpDTO(String userId, String password) {
     this.userId = userId;
---
+++ src/test/java/com/hn/api/diary/service/SignServiceTest.java
@@ -0,0 +1,7 @@
+package com.hn.api.diary.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SignServiceTest {
+
+}
