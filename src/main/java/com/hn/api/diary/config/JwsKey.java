4b400a310042368b1cdb2036474a6477632566f9
unknown <nami0879@naver.com>
2023-11-04 19:33:33 +0900

    jwsKey set expiration time

--- src/main/java/com/hn/api/diary/config/JwsKey.java
+++ src/main/java/com/hn/api/diary/config/JwsKey.java
@@ -8,7 +8,7 @@ import javax.crypto.SecretKey;
 public class JwsKey {
 
     // todo: 외부 저장소를 사용하여 키를 관리하는 것이 효율적.
-    public static volatile SecretKey key;
+    private static volatile SecretKey key;
     public static SecretKey getJwsSecretKey(){
         if(key == null){
             synchronized (JwsKey.class){
