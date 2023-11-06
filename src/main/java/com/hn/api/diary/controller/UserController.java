58f6f5bb229eea9f61dbfe05fa2423a64d3d639d
unknown <nami0879@naver.com>
2023-11-06 21:23:51 +0900

    crypt password

--- src/main/java/com/hn/api/diary/controller/UserController.java
+++ src/main/java/com/hn/api/diary/controller/UserController.java
@@ -3,33 +3,22 @@ package com.hn.api.diary.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hn.api.diary.config.AuthSession;
-import com.hn.api.diary.config.JwsKey;
-import com.hn.api.diary.dto.SessionDTO;
 import com.hn.api.diary.dto.SignInDTO;
 import com.hn.api.diary.dto.SignUpDTO;
-import com.hn.api.diary.entity.User;
 import com.hn.api.diary.response.SessionResponse;
 import com.hn.api.diary.service.UserService;
-import io.jsonwebtoken.Jwts;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import javax.crypto.SecretKey;
-import java.time.Duration;
-import java.util.Base64;
-import java.util.Date;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RequiredArgsConstructor
 @RestController
 public class UserController {
-
-    @Autowired final private ObjectMapper objectMapper;
 
     private final UserService userService;
 
@@ -40,25 +29,7 @@ public class UserController {
 
     @PostMapping(value = "/signIn")
     public ResponseEntity<SessionResponse> signIn(@RequestBody SignInDTO signInDTO) throws JsonProcessingException {
-        User user = userService.signIn(signInDTO);
-
-        SessionDTO sessionDTO = SessionDTO.builder()
-                .id(user.getId())
-                .email(user.getEmail())
-                .build();
-        String jwtSubject = objectMapper.writeValueAsString(sessionDTO);
-
-        SecretKey key = JwsKey.getJwsSecretKey();
-
-        Date generateDate = new Date();
-        Date expirateDate = new Date(generateDate.getTime() + (60 * 1000));
-
-        String jws = Jwts.builder()
-                .subject(jwtSubject)
-                .signWith(key)
-                .issuedAt(generateDate)
-                .expiration(expirateDate)
-                .compact();
+        String jws = userService.signIn(signInDTO);
 
         HttpHeaders headers = new HttpHeaders();
         headers.add(HttpHeaders.AUTHORIZATION, jws);
@@ -68,12 +39,11 @@ public class UserController {
         return ResponseEntity.ok()
                 .headers(headers)
                 .body(body);
-
     }
 
     @PostMapping(value = "/testAuth")
     public String testAuth(AuthSession authSession) throws JsonProcessingException {
-        return objectMapper.writeValueAsString(authSession);
+        return new ObjectMapper().writeValueAsString(authSession);
     }
 
     @PostMapping(value = "/test")
--- src/main/java/com/hn/api/diary/service/UserService.java
+++ src/main/java/com/hn/api/diary/service/UserService.java
@@ -1,41 +1,79 @@
 package com.hn.api.diary.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hn.api.diary.config.JwsKey;
+import com.hn.api.diary.crypto.PasswordEncoder;
+import com.hn.api.diary.dto.SessionDTO;
 import com.hn.api.diary.dto.SignInDTO;
 import com.hn.api.diary.dto.SignUpDTO;
 import com.hn.api.diary.entity.User;
+import com.hn.api.diary.exception.AlreadyReported;
 import com.hn.api.diary.exception.InvalidValue;
 import com.hn.api.diary.repository.UserRepository;
+import io.jsonwebtoken.Jwts;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+    @Autowired final private ObjectMapper objectMapper;
 
     public void signUp(SignUpDTO signUpDTO){
 
-        // todo: 비밀번호 암호화.
+        Optional<User> optionalUser = userRepository.findByEmail(signUpDTO.getEmail());
+        if(optionalUser.isPresent()){
+            throw new AlreadyReported();
+        }
+
+        String encryptedPassword = PasswordEncoder.encrypt(signUpDTO.getPassword());
+
         User user = User.builder()
                 .email(signUpDTO.getEmail())
-                .password(signUpDTO.getPassword())
+                .password(encryptedPassword)
                 .build();
 
         userRepository.save(user);
     }
 
-    @Transactional
-    public User signIn(SignInDTO signInDTO) {
+    public String signIn(SignInDTO signInDTO) throws JsonProcessingException {
         String receivedEmail = signInDTO.getEmail();
         String receivedPassword = signInDTO.getPassword();
 
-        User user = userRepository.findByEmailAndPassword(receivedEmail, receivedPassword)
+        User user = userRepository.findByEmail(receivedEmail)
                 .orElseThrow(InvalidValue::new);
 
-        // todo: Transactional이 없으면 addSession 실패. 이유 공부.
-//        MySession mySession = user.addSession();
-        return user;
+        var isMatches = PasswordEncoder.matches(receivedPassword, user.getPassword());
+        if(!isMatches)throw new InvalidValue();
+
+        SessionDTO sessionDTO = SessionDTO.builder()
+                .id(user.getId())
+                .email(user.getEmail())
+                .build();
+
+        String jwtSubject = objectMapper.writeValueAsString(sessionDTO);
+
+        SecretKey key = JwsKey.getJwsSecretKey();
+
+        Date generateDate = new Date();
+        Date expirateDate = new Date(generateDate.getTime() + (60 * 1000));
+
+        String jws = Jwts.builder()
+                .subject(jwtSubject)
+                .signWith(key)
+                .issuedAt(generateDate)
+                .expiration(expirateDate)
+                .compact();
+
+        return jws;
     }
 }
