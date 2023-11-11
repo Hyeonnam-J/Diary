25b8e4d12bed221f7f538c0f57230c2f8b8b6042
unknown <nami0879@naver.com>
2023-11-11 18:22:51 +0900

    security signIn... ing...

--- src/main/java/com/hn/api/diary/config/SecurityConfig.java
+++ src/main/java/com/hn/api/diary/config/SecurityConfig.java
@@ -3,18 +3,12 @@ package com.hn.api.diary.config;
 import com.hn.api.diary.entity.User;
 import com.hn.api.diary.exception.InvalidValue;
 import com.hn.api.diary.repository.UserRepository;
-import com.sun.security.auth.UserPrincipal;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -22,38 +16,28 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.util.AntPathMatcher;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
-import java.util.List;
 
 import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @RequiredArgsConstructor
-@EnableWebSecurity(debug = true)
+@EnableWebSecurity(debug = false)
 @Configuration
 public class SecurityConfig {
 
-    @Autowired private UserRepository userRepository;
+    private final UserRepository userRepository;
 
-//    @Bean
+    //    @Bean
 //    public CorsConfigurationSource corsConfigurationSource(){
 //        CorsConfiguration config = new CorsConfiguration();
 //        config.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
 //        config.setAllowedMethods(Arrays.asList("*"));
 //        config.setExposedHeaders(Arrays.asList("Authorization"));
-//        config.setAllowedHeaders(Arrays.asList("*"));
 //
 //        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 //        source.registerCorsConfiguration("/**", config);
@@ -64,6 +48,7 @@ public class SecurityConfig {
     public WebSecurityCustomizer webSecurityCustomizer() {
         return web -> web.ignoring()
                 .requestMatchers(new AntPathRequestMatcher("/favicon.ico"))
+//                .requestMatchers(new AntPathRequestMatcher("/error"))
                 .requestMatchers(toH2Console());
     }
 
@@ -72,21 +57,22 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 // Security - CorsConfigurationSource 대신 MVC - addCorsMappings 사용.
-                // 이걸 지우고 CorsConfigurationSource 써도 되는데 설정이 잘 안 먹는 느낌.
+                // 공식 문서에 .cors(cors -> cors.disable()) 로 통합 설정을 비활성화하고,
+                // CorsConfigurationSource 써도 되는데 설정이 안 먹음.
                 .cors(withDefaults())
                 .authorizeHttpRequests((req) ->
                         req
-                                .requestMatchers(new AntPathRequestMatcher("/postTest")).permitAll()
-                                .requestMatchers(new AntPathRequestMatcher("/signUp")).permitAll()
+                                .requestMatchers(AntPathRequestMatcher.antMatcher("/signUp")).permitAll()
                                 .anyRequest().authenticated()
                 )
                 .addFilterBefore(authFilter(), UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
 
     @Bean
     public AuthFilter authFilter(){
-        AuthFilter authFilter = new AuthFilter("/signIn");
+        AuthFilter authFilter = new AuthFilter();
         authFilter.setAuthenticationManager(authenticationManager());
         return authFilter;
     }
@@ -101,7 +87,8 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+//        return new BCryptPasswordEncoder();
+        return NoOpPasswordEncoder.getInstance();
     }
 
     @Bean
@@ -112,10 +99,13 @@ public class SecurityConfig {
                 User user = userRepository.findByEmail(email)
                         .orElseThrow(InvalidValue::new);
 
-                return org.springframework.security.core.userdetails.User.builder()
+                UserDetails userDetails = org.springframework.security.core.userdetails.User.builder()
                         .username(user.getEmail())
                         .password(user.getPassword())
+                        .roles("USER")
                         .build();
+
+                return userDetails;
             }
         };
     }
