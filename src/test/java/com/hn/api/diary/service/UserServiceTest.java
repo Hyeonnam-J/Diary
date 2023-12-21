02a4ea979cae723fc9f495ecba0e599404998266
unknown <nami0879@naver.com>
2023-12-22 08:40:31 +0900

    test //

--- src/test/java/com/hn/api/diary/service/UserServiceTest.java
+++ src/test/java/com/hn/api/diary/service/UserServiceTest.java
@@ -1,116 +1,116 @@
-package com.hn.api.diary.service;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.hn.api.diary.dto.user.CheckDuplicationDTO;
-import com.hn.api.diary.dto.user.SignUpDTO;
-import com.hn.api.diary.entity.User;
-import com.hn.api.diary.exception.AlreadyReported;
-import com.hn.api.diary.exception.InvalidValue;
-import com.hn.api.diary.repository.FreeBoardCommentRepository;
-import com.hn.api.diary.repository.FreeBoardPostRepository;
-import com.hn.api.diary.repository.UserRepository;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-@SpringBootTest
-class UserServiceTest {
-
-    @Autowired
-    private ObjectMapper objectMapper;
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-    @Autowired
-    private UserService userService;
-    @Autowired
-    private UserRepository userRepository;
-    @Autowired
-    private FreeBoardPostRepository freeBoardPostRepository;
-    @Autowired
-    private FreeBoardCommentRepository freeBoardCommentRepository;
-
-    @BeforeEach
-    void clean() {
-        freeBoardCommentRepository.deleteAll();
-        freeBoardPostRepository.deleteAll();
-        userRepository.deleteAll();
-    }
-
-    // signUp() - start
-    /* ********************************************************************************* */
-    @Test
-    @DisplayName("sign up : fail while check duplicated email")
-    void failWhileCheckDuplicatedEmail() {
-        // [given]
-
-        // 데이터를 넣어두고,
-        User user = User.builder()
-                .email("testDuplicated")
-                .password("any")
-                .userName("userName")
-                .build();
-        userRepository.save(user);
-
-        // signUp 로직을 실행하기 위한 파라미터 생성.
-        SignUpDTO signUpDTO = SignUpDTO.builder()
-                .email("testDuplicated")
-                .password("any")
-                .userName("userName")
-                .build();
-
-        // [expected]
-        Assertions.assertThrows(AlreadyReported.class, () -> userService.signUp(signUpDTO));
-    }
-
-    @Test
-    @DisplayName("sign up : check encrypted password")
-    void checkEncryptedPassword() {
-        // [given]
-
-        // 암호화하는 signUp 로직을 거쳐 저장하고,
-        SignUpDTO signUpDTO = SignUpDTO.builder()
-                .email("any")
-                .password("isEncrypted")
-                .userName("userName")
-                .build();
-        userService.signUp(signUpDTO);
-
-        // 저장된 데이터를 가져온다.
-        User user = userRepository.findByEmail("any")
-                .orElseThrow(InvalidValue::new);
-
-        // [expected]
-        Assertions.assertTrue(passwordEncoder.matches(signUpDTO.getPassword(), user.getPassword()));
-    }
-
-    @Test
-    @DisplayName("sign up : check duplicated value")
-    public void checkDuplicatedValue() {
-        // [given]
-        User user = User.builder()
-                .email("email@naver.com")
-                .password("!@#123QWEqwe")
-                .nick("nick")
-                .build();
-        userRepository.save(user);
-
-        CheckDuplicationDTO checkDuplicationDTO_1 = CheckDuplicationDTO.builder()
-                .item("email")
-                .value("email@naver.com")
-                .build();
-        CheckDuplicationDTO checkDuplicationDTO_2 = CheckDuplicationDTO.builder()
-                .item("nick")
-                .value("nick")
-                .build();
-
-        // [expected]
-        Assertions.assertThrows(AlreadyReported.class, () -> userService.checkDuplication(checkDuplicationDTO_1));
-        Assertions.assertThrows(AlreadyReported.class, () -> userService.checkDuplication(checkDuplicationDTO_2));
-    }
-    /* ********************************************************************************* */
-    // signUp() - end
-}
+//package com.hn.api.diary.service;
+//
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.hn.api.diary.dto.user.CheckDuplicationDTO;
+//import com.hn.api.diary.dto.user.SignUpDTO;
+//import com.hn.api.diary.entity.User;
+//import com.hn.api.diary.exception.AlreadyReported;
+//import com.hn.api.diary.exception.InvalidValue;
+//import com.hn.api.diary.repository.FreeBoardCommentRepository;
+//import com.hn.api.diary.repository.FreeBoardPostRepository;
+//import com.hn.api.diary.repository.UserRepository;
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//
+//@SpringBootTest
+//class UserServiceTest {
+//
+//    @Autowired
+//    private ObjectMapper objectMapper;
+//    @Autowired
+//    private PasswordEncoder passwordEncoder;
+//    @Autowired
+//    private UserService userService;
+//    @Autowired
+//    private UserRepository userRepository;
+//    @Autowired
+//    private FreeBoardPostRepository freeBoardPostRepository;
+//    @Autowired
+//    private FreeBoardCommentRepository freeBoardCommentRepository;
+//
+//    @BeforeEach
+//    void clean() {
+//        freeBoardCommentRepository.deleteAll();
+//        freeBoardPostRepository.deleteAll();
+//        userRepository.deleteAll();
+//    }
+//
+//    // signUp() - start
+//    /* ********************************************************************************* */
+//    @Test
+//    @DisplayName("sign up : fail while check duplicated email")
+//    void failWhileCheckDuplicatedEmail() {
+//        // [given]
+//
+//        // 데이터를 넣어두고,
+//        User user = User.builder()
+//                .email("testDuplicated")
+//                .password("any")
+//                .userName("userName")
+//                .build();
+//        userRepository.save(user);
+//
+//        // signUp 로직을 실행하기 위한 파라미터 생성.
+//        SignUpDTO signUpDTO = SignUpDTO.builder()
+//                .email("testDuplicated")
+//                .password("any")
+//                .userName("userName")
+//                .build();
+//
+//        // [expected]
+//        Assertions.assertThrows(AlreadyReported.class, () -> userService.signUp(signUpDTO));
+//    }
+//
+//    @Test
+//    @DisplayName("sign up : check encrypted password")
+//    void checkEncryptedPassword() {
+//        // [given]
+//
+//        // 암호화하는 signUp 로직을 거쳐 저장하고,
+//        SignUpDTO signUpDTO = SignUpDTO.builder()
+//                .email("any")
+//                .password("isEncrypted")
+//                .userName("userName")
+//                .build();
+//        userService.signUp(signUpDTO);
+//
+//        // 저장된 데이터를 가져온다.
+//        User user = userRepository.findByEmail("any")
+//                .orElseThrow(InvalidValue::new);
+//
+//        // [expected]
+//        Assertions.assertTrue(passwordEncoder.matches(signUpDTO.getPassword(), user.getPassword()));
+//    }
+//
+//    @Test
+//    @DisplayName("sign up : check duplicated value")
+//    public void checkDuplicatedValue() {
+//        // [given]
+//        User user = User.builder()
+//                .email("email@naver.com")
+//                .password("!@#123QWEqwe")
+//                .nick("nick")
+//                .build();
+//        userRepository.save(user);
+//
+//        CheckDuplicationDTO checkDuplicationDTO_1 = CheckDuplicationDTO.builder()
+//                .item("email")
+//                .value("email@naver.com")
+//                .build();
+//        CheckDuplicationDTO checkDuplicationDTO_2 = CheckDuplicationDTO.builder()
+//                .item("nick")
+//                .value("nick")
+//                .build();
+//
+//        // [expected]
+//        Assertions.assertThrows(AlreadyReported.class, () -> userService.checkDuplication(checkDuplicationDTO_1));
+//        Assertions.assertThrows(AlreadyReported.class, () -> userService.checkDuplication(checkDuplicationDTO_2));
+//    }
+//    /* ********************************************************************************* */
+//    // signUp() - end
+//}
