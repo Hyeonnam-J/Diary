3237a36b8469d71c05c6841bfddc49677afd8ee9
unknown <nami0879@naver.com>
2023-12-22 04:53:41 +0900

    test

--- src/main/java/com/hn/api/diary/controller/TestController.java
+++ src/main/java/com/hn/api/diary/controller/TestController.java
@@ -1,15 +1,12 @@
 package com.hn.api.diary.controller;
 
-import com.hn.api.diary.entity.FreeBoardComment;
-import com.hn.api.diary.entity.FreeBoardPost;
-import com.hn.api.diary.repository.FreeBoardCommentRepository;
-import com.hn.api.diary.repository.FreeBoardPostRepository;
-import com.hn.api.diary.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.hn.api.diary.repository.FreeBoardCommentRepository;
+import com.hn.api.diary.repository.FreeBoardPostRepository;
+import com.hn.api.diary.repository.UserRepository;
 
 @RestController
 public class TestController {
