3d9aca6d01edba920a8a6377188af0a358a5bdfe
unknown <nami0879@naver.com>
2023-12-21 13:12:55 +0900

    project name

--- src/main/java/com/hn/api/diary/DiaryApiApplication.java
+++ src/main/java/com/hn/api/diary/DiaryApiApplication.java
@@ -6,10 +6,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-public class DiaryApplication {
+public class DiaryApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DiaryApplication.class, args);
+        SpringApplication.run(DiaryApiApplication.class, args);
     }
 
 }
