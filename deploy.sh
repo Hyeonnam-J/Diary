392d1da85601e7ea353eaa3fd0969a53cbe99398
unknown <nami0879@naver.com>
2023-12-24 05:41:23 +0900

    tt2

--- deploy.sh
+++ deploy.sh
@@ -3,15 +3,15 @@
 REPOSITORY=/home/ec2-user
 cd $REPOSITORY
 
-APP_NAME=demo
-JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
-JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+APP_NAME=diary-api
+JAR_NAME=$(ls $REPOSITORY | grep '.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/$JAR_NAME
 
 
 # =====================================
 # 현재 구동 중인 application pid 확인
 # =====================================
-CURRENT_PID=$(pgrep -fl demo | grep java | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl diary-api | grep java | awk '{print $1}')
 
 if [ -z "$CURRENT_PID" ]; then
     echo "NOT RUNNING"
