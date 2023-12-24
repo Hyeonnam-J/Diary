2cff93b9bafc3a440eec80d7c8fd1c97db934a02
unknown <nami0879@naver.com>
2023-12-24 15:23:30 +0900

    complete deploy and run

--- src/main/java/com/hn/api/diary/config/WebConfig.java
+++ src/main/java/com/hn/api/diary/config/WebConfig.java
@@ -13,9 +13,9 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedOrigins("*")
-//                .allowedOrigins("http://localhost:3000")
-//                .allowedOrigins("https://main.du6t0lf6rbu8z.amplifyapp.com/")
+            //     .allowedOrigins("*")
+            //    .allowedOrigins("http://localhost:3000")
+               .allowedOrigins("https://main.du6t0lf6rbu8z.amplifyapp.com/")
                 .allowedHeaders("*")    // 클라이언트가 요청할 때 허용되는 헤더.
                 .exposedHeaders("Authorization");   // 서버에서 응답했을 때 브라우저가 접근 가능한 헤더.
     }
