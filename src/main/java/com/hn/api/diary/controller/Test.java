0f665182aed5593066e0d2f6369205ecb3adc869
unknown <nami0879@naver.com>
2024-01-04 04:20:31 +0900

    logback until fileAppender

---
+++ src/main/java/com/hn/api/diary/controller/Test.java
@@ -0,0 +1,14 @@
+package com.hn.api.diary.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Test {
+
+     static final Logger logger = LoggerFactory.getLogger(Test.class);
+
+    public void test(){
+        logger.info("jhn test class...");
+    }
+
+}
--- src/main/java/com/hn/api/diary/controller/TestController.java
+++ src/main/java/com/hn/api/diary/controller/TestController.java
@@ -1,12 +1,13 @@
 package com.hn.api.diary.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.hn.api.diary.repository.FreeBoardCommentRepository;
 import com.hn.api.diary.repository.FreeBoardPostRepository;
 import com.hn.api.diary.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
@@ -15,8 +16,12 @@ public class TestController {
     @Autowired private FreeBoardPostRepository freeBoardPostRepository;
     @Autowired private FreeBoardCommentRepository freeBoardCommentRepository;
 
+    static final Logger logger = LoggerFactory.getLogger(TestController.class);
+
     @GetMapping(value = "/test")
     public String test(){
+        new Test().test();
+        logger.info("jhn test controller class ...");
         return "테스트22";
     }
 }
