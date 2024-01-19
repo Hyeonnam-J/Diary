6a60957073c0dee78ddf17f94cb2024240d175bd
unknown <nami0879@naver.com>
2024-01-19 16:57:51 +0900

    conf

--- src/main/java/com/hn/api/diary/config/WebConfig.java
+++ src/main/java/com/hn/api/diary/config/WebConfig.java
@@ -18,7 +18,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("*")
 //                .allowedOrigins("http://localhost:3000")
 //                .allowedOrigins("https://main.du6t0lf6rbu8z.amplifyapp.com")
-                .allowCredentials(true)
+//                .allowCredentials(true)
 //                .exposedHeaders("*")   // 클라이언트가 받을 수 있는,
 //                .allowedHeaders("*");   // 클라이언트가 요청할 수 있는,
                 .exposedHeaders("Set-Cookie")   // 클라이언트가 받을 수 있는,
