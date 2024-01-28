6fabee20afc86be4c3d902cc420aa1e0339c7c85
unknown <nami0879@naver.com>
2024-01-28 12:54:24 +0900

    headers

--- src/main/java/com/hn/api/diary/filter/SignInFilter.java
+++ src/main/java/com/hn/api/diary/filter/SignInFilter.java
@@ -98,6 +98,8 @@ public class SignInFilter extends AbstractAuthenticationProcessingFilter {
                 .build();
         response.setHeader("Set-Cookie", cookie.toString());
 
+        response.setHeader("test", "test");
+
         response.setStatus(HttpServletResponse.SC_OK);
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
 
