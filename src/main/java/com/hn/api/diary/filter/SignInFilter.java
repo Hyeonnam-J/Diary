b033fa440d7683cd195a533f3e5e6abaf6442a99
unknown <nami0879@naver.com>
2024-01-29 20:36:36 +0900

    set domain

--- src/main/java/com/hn/api/diary/filter/SignInFilter.java
+++ src/main/java/com/hn/api/diary/filter/SignInFilter.java
@@ -95,8 +95,7 @@ public class SignInFilter extends AbstractAuthenticationProcessingFilter {
                 .sameSite("None")
                 .maxAge((int) (expireDate.getTime() - generateDate.getTime()) / 1000)
                 .secure(true)
-                .httpOnly(false)
-                .domain("my-diary.life")
+                .domain("api.my-diary.life")
                 .build();
         response.setHeader("Set-Cookie", cookie.toString());
 
