4343141d2c4230c895b663a6ef061533d7591059
unknown <nami0879@naver.com>
2024-01-31 15:25:21 +0900

    authorization -> cookie

--- src/main/java/com/hn/api/diary/filter/SignInFilter.java
+++ src/main/java/com/hn/api/diary/filter/SignInFilter.java
@@ -96,6 +96,8 @@ public class SignInFilter extends AbstractAuthenticationProcessingFilter {
                 .build();
         response.setHeader("Set-Cookie", cookie.toString());
 
+        System.out.println(cookie);
+
         response.setStatus(HttpServletResponse.SC_OK);
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
 
