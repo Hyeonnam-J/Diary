26728c1ce87a373da751812c8ea957a5dafe74b3
unknown <nami0879@naver.com>
2024-02-04 08:57:42 +0900

    test log

--- src/main/java/com/hn/api/diary/DiaryApiApplication.java
+++ src/main/java/com/hn/api/diary/DiaryApiApplication.java
@@ -10,6 +10,7 @@ public class DiaryApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DiaryApiApplication.class, args);
+        System.out.println("오늘 1");
     }
 
 }
