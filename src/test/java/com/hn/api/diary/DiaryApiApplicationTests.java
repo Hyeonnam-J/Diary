80688096cf3b181a13fc1aa4fe0d125cc2783549
unknown <nami0879@naver.com>
2024-02-10 10:55:02 +0900

    seperate yml for test

--- src/test/java/com/hn/api/diary/DiaryApiApplicationTests.java
+++ src/test/java/com/hn/api/diary/DiaryApiApplicationTests.java
@@ -1,8 +1,15 @@
 package com.hn.api.diary;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest
 class DiaryApiApplicationTests {
