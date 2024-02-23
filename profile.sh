4b94858e8c4563a51b64ecd370b002b723d5e366
unknown <nami0879@naver.com>
2024-02-24 08:28:15 +0900

    zdd...

--- profile.sh
+++ profile.sh
@@ -4,38 +4,38 @@
 # set1이 사용 중이면 set2가 쉬고 있으며, 반대편 set1이 쉬고 있다.
 function find_idle_profile()
 {
-#    RESPONSE_CODE=$(sudo curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
-#
-#    if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면 (즉, 40x/50x 에러 모두 포함)
-#    then
-#        CURRENT_PROFILE=set2
-#    else
-#        CURRENT_PROFILE=$(sudo curl -s http://localhost/profile)
-#    fi
-#
-#    if [ ${CURRENT_PROFILE} == set1 ]
-#    then
-#        IDLE_PROFILE=set2 # Nginx와 연결되지 않은 profile
-#    else
-#        IDLE_PROFILE=set1
-#    fi
-#
-#    # bash script는 return 기능이 없기 떄문에,
-#    # echo를 통해서 출력하면 이 값을 클라이언트가 사용할 수 있습니다.
-#    echo "${IDLE_PROFILE}"
+    RESPONSE_CODE=$(sudo curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
 
-    echo "profile.sh-find_idle_profile"
-
-    CURRENT_PORT=$(sudo netstat -tnlp | grep LISTEN | awk '{print $4}' | awk -F: '{print $2}' | sort -n | uniq)
-
-    if [ ${CURRENT_PORT} -eq 8081 ]
+    if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면 (즉, 40x/50x 에러 모두 포함)
     then
-        IDLE_PROFILE=set2
+        CURRENT_PROFILE=set2
+    else
+        CURRENT_PROFILE=$(sudo curl -s http://localhost/profile)
+    fi
+
+    if [ ${CURRENT_PROFILE} == set1 ]
+    then
+        IDLE_PROFILE=set2 # Nginx와 연결되지 않은 profile
     else
         IDLE_PROFILE=set1
     fi
 
+    # bash script는 return 기능이 없기 떄문에,
+    # echo를 통해서 출력하면 이 값을 클라이언트가 사용할 수 있습니다.
     echo "${IDLE_PROFILE}"
+
+#    echo "profile.sh-find_idle_profile"
+#
+#    CURRENT_PORT=$(sudo netstat -tnlp | grep LISTEN | awk '{print $4}' | awk -F: '{print $2}' | sort -n | uniq)
+#
+#    if [ ${CURRENT_PORT} -eq 8081 ]
+#    then
+#        IDLE_PROFILE=set2
+#    else
+#        IDLE_PROFILE=set1
+#    fi
+#
+#    echo "${IDLE_PROFILE}"
 }
 
 # 쉬고 있는 profile의 port 찾기
