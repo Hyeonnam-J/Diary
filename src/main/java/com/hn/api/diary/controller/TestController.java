dd430fa4d70eeeacfaee0df5bb838b8c95f7b42f
unknown <nami0879@naver.com>
2024-02-24 15:27:22 +0900

    opti...ing deploy.sh

--- src/main/java/com/hn/api/diary/controller/TestController.java
+++ src/main/java/com/hn/api/diary/controller/TestController.java
@@ -14,7 +14,7 @@ public class TestController {
 
     static final Logger logger = LoggerFactory.getLogger(TestController.class);
 
-    static String test = "test-13";
+    static String test = "test-14";
 
     @GetMapping(value = "/test")
     public String test(){
