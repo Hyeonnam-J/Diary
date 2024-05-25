b613345fcfa55f73fad324d51a2fef86b5b7b6d6
unknown <nami0879@naver.com>
2024-05-25 18:01:52 +0900

    test

--- src/main/java/com/hn/api/diary/controller/TestController.java
+++ src/main/java/com/hn/api/diary/controller/TestController.java
@@ -14,7 +14,7 @@ public class TestController {
 
     static final Logger logger = LoggerFactory.getLogger(TestController.class);
 
-    static String test = "test-200";
+    static String test = "test-201";
 
     @GetMapping(value = "/test")
     public String test(){
